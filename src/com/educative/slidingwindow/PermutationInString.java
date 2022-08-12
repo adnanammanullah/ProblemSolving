1e3f8db77c646acc1326e3ad27a86e8718eb091e
Adnan Amman Ullah <mianadnan32@gmail.com>
2022-08-13 04:54:49 +0500

    Add files via upload

---
+++ src/com/educative/slidingwindow/PermutationInString.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+
+public class PermutationInString {
+	
+	public static boolean findPermutation(String str, String pattern) {
+		
+		HashMap<Character, Integer> charCount = new HashMap<>();
+		//fill hashmap
+		for (Character c : pattern.toCharArray()) {
+			charCount.put(c, charCount.getOrDefault(c, 0)+1);
+		}
+		
+		boolean isPermuteExist=false;
+		int windowStart=0;
+		int matched=0;
+		
+		for(int windowEnd=0; windowEnd<str.length(); windowEnd++) {
+			//keep moving with charCount update
+			if(charCount.containsKey(str.charAt(windowEnd))) {
+				charCount.put(str.charAt(windowEnd), charCount.getOrDefault(str.charAt(windowEnd), 0)-1);
+				
+				if(charCount.getOrDefault(str.charAt(windowEnd), 0)==0)
+					matched++;
+			}
+			
+			if(matched==charCount.size())
+				return true;
+			
+			while(windowEnd-windowStart+1 > pattern.length()) {
+				//shrink the window
+				if(charCount.containsKey(str.charAt(windowStart))) {
+					if(charCount.getOrDefault(str.charAt(windowStart), 0)==0)
+						matched--;
+					charCount.put(str.charAt(windowEnd), charCount.getOrDefault(str.charAt(windowEnd), 0)+1);
+				}
+				windowStart++;
+			}
+		}
+		
+		return isPermuteExist;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(findPermutation("oidbcaf", "abc"));
+		System.out.println(findPermutation("odicf", "dc"));
+		System.out.println(findPermutation("bcdxabcdy", "bcdyabcdx"));
+		System.out.println(findPermutation("aaacb", "abc"));
+		System.out.println(findPermutation("a", "xyd"));
+	}
+
+}
