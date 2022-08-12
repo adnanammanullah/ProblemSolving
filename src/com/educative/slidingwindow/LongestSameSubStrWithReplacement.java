53aeacb1fca19c99731187a6fc730d07b3dc9443
Adnan Amman Ullah <mianadnan32@gmail.com>
2022-08-12 11:42:37 +0500

    fixing the solution

--- src/com/educative/slidingwindow/LongestSameSubStrWithReplacement.java
+++ src/com/educative/slidingwindow/LongestSameSubStrWithReplacement.java
@@ -5,28 +5,24 @@ public class LongestSameSubStrWithReplacement {
 	public static int findLength(String str, int k) {
 		
 		int maxLen = Integer.MIN_VALUE;
-		int kk=k;
 		int windowStart=0;
-		int firstRepIndex=0;
+		int maxCharCount = Integer.MIN_VALUE;
+		HashMap<Character, Integer> charCount = new HashMap<>();
 		
 		for(int windowEnd=0; windowEnd<str.length(); windowEnd++) {
- 			while((windowEnd<str.length() && str.charAt(windowStart)==str.charAt(windowEnd)) || kk>0) {
-				if(str.charAt(windowStart)!=str.charAt(windowEnd)) {
-					kk--;
-					if(firstRepIndex==0) {
-						firstRepIndex=windowEnd;
-					}
-				}
-				
-				if(maxLen<windowEnd-windowStart+1) {
-					maxLen=windowEnd-windowStart+1;
-				}
-				windowEnd++;
-			}
-			
-			windowStart=firstRepIndex;
-			firstRepIndex=0;
-			kk=k;
+ 			charCount.put(str.charAt(windowEnd), charCount.getOrDefault(str.charAt(windowEnd), 0)+1);
+ 			if(maxCharCount<charCount.getOrDefault(str.charAt(windowEnd), 0))
+ 				maxCharCount = charCount.getOrDefault(str.charAt(windowEnd), 0);
+ 			
+ 			while(windowEnd-windowStart+1-maxCharCount > k ) {
+ 				charCount.put(str.charAt(windowStart), charCount.getOrDefault(str.charAt(windowStart), 0)-1);
+ 				
+ 				if(charCount.getOrDefault(str.charAt(windowStart), 0)==0)
+ 					charCount.remove(str.charAt(windowStart));
+ 				windowStart++;
+ 			}
+ 			if(windowEnd-windowStart+1 > maxLen)
+ 				maxLen = windowEnd-windowStart+1;  
 		}
 		
 		return maxLen==Integer.MIN_VALUE?-1:maxLen;
