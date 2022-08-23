fec5954b8715db582cc71d1d6a75b09e200c4849
Adnan Amman Ullah <mianadnan32@gmail.com>
2022-08-23 22:44:07 +0500

    fixed solution

--- src/com/educative/fastSlowPointer/RearrangeLinkedList.java
+++ src/com/educative/fastSlowPointer/RearrangeLinkedList.java
@@ -1,5 +1,8 @@
 package com.educative.fastSlowPointer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class RearrangeLinkedList {
 
 	public static ListNode findMiddle(ListNode head) {
@@ -26,29 +29,47 @@ class RearrangeLinkedList {
 
 	public static void reorder(ListNode head) {
 		// TODO: Write your code here
-
+		ListNode headOfFirstHalf = head;
 		ListNode middleNode = findMiddle(head);
-		ListNode reverseHead = reverseList(middleNode);
-		ListNode reverseHeadCopy = reverseHead;
-		while (head != null) {
-			ListNode next = head.next;
-			head.next=reverseHead;
-			
-			head=next;
-			reverseHead=reverseHead.next;
-		}
-
+		ListNode headOfSecondHalf = reverseList(middleNode);
 		
+		while (headOfFirstHalf != null && headOfSecondHalf!=null) {
+			ListNode next1 = headOfFirstHalf.next;
+			headOfFirstHalf.next=headOfSecondHalf;
+			headOfFirstHalf=next1;
+			
+			ListNode next2 = headOfSecondHalf.next;
+			headOfSecondHalf.next = headOfFirstHalf;
+			headOfSecondHalf=next2;
+		}
+		
+		if(headOfFirstHalf != null)
+			headOfFirstHalf.next = null;
 	}
-
+	
 	public static void main(String[] args) {
 		ListNode head = new ListNode(2);
 		head.next = new ListNode(4);
 		head.next.next = new ListNode(6);
 		head.next.next.next = new ListNode(8);
 		head.next.next.next.next = new ListNode(10);
-		head.next.next.next.next.next = new ListNode(12);
+//		head.next.next.next.next.next = new ListNode(12);
+
+		List<String> arr = new ArrayList<>();
+		
+		for(String str: arr) {
+			
+		}
+		arr.remove("a");
+		ListNode headCopy = head;
+		while (headCopy != null) {
+			System.out.print(headCopy.value + " ");
+			headCopy = headCopy.next;
+		}
+		
+		System.out.println();
 		reorder(head);
+		
 		while (head != null) {
 			System.out.print(head.value + " ");
 			head = head.next;
