29bd19aa630ddb5bb9505dc32979a0ac1d7ed080
Adnan Amman Ullah <mianadnan32@gmail.com>
2022-09-04 02:29:44 +0500

    adding new problem statement

---
+++ src/com/educative/cyclicSort/MissingNumber.java
@@ -0,0 +1,30 @@
+package com.educative.cylicSort;
+
+public class MissingNumber {
+	
+	 public static int findMissingNumber(int[] nums) {
+		 int temp=-1;
+		 int missingNum=-1;
+		 for(int i=0; i< nums.length; ) {
+			 if(nums[i]<nums.length && nums[i]!=i) {
+				 temp=nums[nums[i]];
+				 nums[nums[i]]=nums[i];
+				 nums[i]=temp;
+			 }else
+				 i++;
+		 }
+		 for(int i=0; i<nums.length; i++) {
+			 if(nums[i]!=i) {
+				 return i;
+			 }
+		 }
+		 return nums.length;
+	 }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(MissingNumber.findMissingNumber(new int[] { 4, 0, 3, 1 }));
+	    System.out.println(MissingNumber.findMissingNumber(new int[] { 8, 3, 5, 2, 4, 6, 0, 1 }));
+	}
+
+}
